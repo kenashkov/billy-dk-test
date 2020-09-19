c0edb769202275a536ec850c10b8735f904ec555
Vesko Kenashkov <vesko@azonmedia.com>
2020-09-19 22:03:29 +0300

    - completed tests of validate_pai_path()

--- tests/unit/BillyDkTest.php
+++ tests/unit/BillyDkTest.php
@@ -17,7 +17,7 @@ class BillyDkTest extends \Codeception\Test\Unit
     {
     }
 
-    // tests
+    // tests each exception in validate_api_path()
     public function test_api_path_validation_1()
     {
         $this->expectExceptionMessageMatches('/No path is provided/');
@@ -34,5 +34,28 @@ class BillyDkTest extends \Codeception\Test\Unit
     {
         $this->expectExceptionMessageMatches('/besides the leading one/');
         \Kenashkov\BillyDk\BillyDk::validate_api_path('/products/');
+    }
+
+    public function test_api_path_validation_4()
+    {
+        $this->expectExceptionMessageMatches('/contains a base path/');
+        \Kenashkov\BillyDk\BillyDk::validate_api_path('/productsWRONG');
+    }
+
+    public function test_api_path_validation_5()
+    {
+        $this->expectExceptionMessageMatches('/not appear to be valid/');
+        \Kenashkov\BillyDk\BillyDk::validate_api_path('/_');
+    }
+
+    //test some valid paths
+    public function test_api_path_validation_ok_1()
+    {
+        \Kenashkov\BillyDk\BillyDk::validate_api_path('/products');
+    }
+
+    public function test_api_path_validation_ok_2()
+    {
+        \Kenashkov\BillyDk\BillyDk::validate_api_path('/products?param1=value1');
     }
 }
